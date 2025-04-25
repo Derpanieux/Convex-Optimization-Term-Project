dda17dd40b56ece7ae2a54f859af255c0528ee8a
Kasey TIan <iamkaseytian@gmail.com>
2025-04-25 17:24:43 -0400

    Wrote linear example code, created figures, and finished subgradient step sizes

---
+++ Code/linear_example.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#dimensions
+n = 10
+m = 25
+
+#generate problem
+a = np.random.normal(size=(m, n))
+b = np.random.normal(size=m)
+
+def f(x):
+    values = np.matvec(a, x) + b
+    return np.max(values)
+
+def g(x):
+    values = np.matvec(a, x) + b
+    return a[np.argmax(values)]
+
+#step sizes
+def constant_step_size(g, k):
+    alpha = 0.01
+    return alpha
+
+def constant_step_len(g, k):
+    gamma = 0.01
+    norm = np.linalg.vector_norm(g)
+    return gamma/norm
+
+def square_sum_not_sum(g, k):
+    a = 1
+    b = 1
+    return a/(b+k)
+
+def nonsum_dim_size(g, k):
+    a = 0.11
+    return a / math.sqrt(k)
+
+def nonsum_dim_len(g,k):
+    a = 0.1
+    gammak = a / math.sqrt(k)
+    norm = np.linalg.vector_norm(g)
+    return gammak/norm
+
+def sgmethod(f, g, x0, step, maxiter):
+    xkvec = [x0]
+    xbest = x0
+    for k in range(1,maxiter+1):
+        xk = xkvec[-1]
+        subgradient = g(xk)
+        alphak = step(subgradient, k)
+        xnext = xk - alphak*subgradient
+        #keep track of best
+        if f(xnext) < f(xbest):
+            xbest = xnext
+        xkvec.append(xnext)
+    print(f"Finished. f* = {f(xbest):.5f}")
+    return xkvec, xbest
+
+
+x0 = np.zeros(n)
+maxiter = 100
+fstar = math.inf
+def format(name, step):
+    global fstar
+    xkvec, xbest = sgmethod(f, g, x0, step, maxiter)
+    thisbest = f(xbest)
+    if thisbest < fstar:
+        fstar = thisbest
+    fkvec = [f(xk) for xk in xkvec]
+    sgvec = [np.linalg.vector_norm(g(xk)) for xk in xkvec]
+    return name, xkvec, fkvec, sgvec
+
+formatted = []
+formatted.append(format("Constant Size", constant_step_size))
+formatted.append(format("Constant Length", constant_step_len))
+formatted.append(format("Sq. Sum. not Sum.", square_sum_not_sum))
+formatted.append(format("Nonsum. Dim. Size", nonsum_dim_size))
+formatted.append(format("Nonsum. Dim. Length", nonsum_dim_len))
+
+karr = [k for k in range(0,maxiter+1)]
+fig, ax = plt.subplots()
+ax.set_title("Value gap of $f(x^{(k)}) - f^*$ against $k$")
+ax.set_ylabel("$f(x^{(k)}) - f^*$")
+ax.set_xlabel("$k$")
+ax.set_yscale('log')
+for entry in formatted:
+    gapvec = entry[2] - fstar
+    ax.plot(karr, gapvec, label=entry[0])
+ax.legend()
+
+fig2, ax2 = plt.subplots()
+ax2.set_title("$\|g^{(k)}\|_2$ against $k$ with " + entry[0])
+ax2.set_ylabel("$\|g^{(k)}\|_2$")
+ax2.set_xlabel("$k$")
+ax2.set_yscale('log')
+for entry in formatted:
+    ax2.plot(karr, entry[3], label=entry[0])
+ax2.legend()
+
+    
+
+plt.show()
